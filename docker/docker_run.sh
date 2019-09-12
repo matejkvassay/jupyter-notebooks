27b59091848dba9ead55b589ff40112942d36b25
Matej Kvassay <matejkvassay@icloud.com>
2019-09-12 14:39:05 +0000

    automatic save

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,17 +1,13 @@
 #config
 export JUPYL_PORT=9996
 export JUPYL_WORKDIR=$HOME/project/mkv-jupyter-notebooks
-export JUPYL_VENV=$HOME/.virtualenvs/jupyter
 export JUPYL_JUPYCFG=$HOME/.jupyter
 export JUPYL_CERT=$HOME/.ssh/mycert.pem
 export JUPYL_KEY=$HOME/.ssh/mykey.key
 
-#set up env permissions
-sudo chmod -R 777 $JUPYL_VENV
-
 #docker run
-sudo docker run --rm \
-    -v $JUPYL_VENV:/opt/venv \
+sudo docker run -d \
+    --restart=unless-stopped \
     -v $JUPYL_JUPYCFG:/root/.jupyter \
     -v $JUPYL_CERT:/opt/mycert.pem \
     -v $JUPYL_KEY:/opt/mykey.key \
